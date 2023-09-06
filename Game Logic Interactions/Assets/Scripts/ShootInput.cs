b19e18454521aebf3edad3789b1070a74656cab6
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-09-05 21:11:04 -0500

    general script clean up

--- Game Logic Interactions/Assets/Scripts/ShootInput.cs
+++ Game Logic Interactions/Assets/Scripts/ShootInput.cs
@@ -15,9 +15,7 @@ public class ShootInput : MonoBehaviour
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
             Shoot();
-        }
     }
 
     private void Shoot()
@@ -25,14 +23,7 @@ public class ShootInput : MonoBehaviour
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
-        {
-            print($"hit {hit.transform.name}");
-
             if (hit.transform.TryGetComponent<AI>(out AI ai))
-            {
                 ai .KillAI();
-                
-            }
-        }
     }
 }
