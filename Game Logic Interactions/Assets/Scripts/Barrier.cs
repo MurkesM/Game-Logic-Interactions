6c2057142e0779c7482ab74d4f7205ff7f5225d5
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-09-10 22:32:11 -0500

    barriers can be destroyed and repaired after a certain time

--- Game Logic Interactions/Assets/Scripts/Barrier.cs
+++ Game Logic Interactions/Assets/Scripts/Barrier.cs
@@ -1,12 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class Barrier : MonoBehaviour, IShootable
 {
     public AudioSource audioSource;
+    public MeshRenderer meshRenderer;
+    public bool canShoot = true;
 
     public void Hit()
     {
+        if (!canShoot)
+            return;
+
+        canShoot = false;
+
         audioSource.Play();
+
+        //turn off barrier
+        meshRenderer.enabled = false;
+
+        StartCoroutine(CoolDownRoutine());
+    }
+
+    public IEnumerator CoolDownRoutine()
+    {
+        yield return new WaitForSeconds(2);
+
+        RepairBarrier();
+    }
+
+    public void RepairBarrier()
+    {
+        canShoot = true;
+
+        //turn off barrier
+        meshRenderer.enabled = true;
     }
 
     public void Shot()
